4bcfeb6c737f82fc62a028b554b5d801f4f8fea8
Nikolainkin <nikolai9710@gmail.com>
2023-04-23 21:31:00 +0300

    Подчистил лишнее

--- GB_PyNote/App/App_01.py
+++
@@ -1,9 +0,0 @@
-﻿from ..Core.Model.presenter import presenter
-
-class app_01:
-
-    def start_app():
-        Pres = presenter()
-        print("START")
-        Pres.print_note()
-        Pres.create_note()
